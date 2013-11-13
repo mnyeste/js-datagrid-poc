9fe561b75b0dd4173c7101ee7123b807457f558d
misi <misi@adnovum.hu>
2013-11-13 09:41:10 +0100

    Edit quantity

--- app/pocgrid.js
+++ app/pocgrid.js
@@ -31,7 +31,8 @@
 			name: "Quantity",
 			field: "quantity",
 			width: 100,
-			cssClass: "numeric"
+			cssClass: "numeric",
+			formatter: editQuantity
 		}, {
 			id: "delete",
 			name: "Delete",
@@ -44,11 +45,17 @@
 			cssClass: "icon",
 			formatter: statusIcon,
 			width: 100
+		}, {
+			id: "internal_id",
+			name: "ID",
+			field: "id",
+			width: 100
 		}];
 
 		var options = {
 			enableColumnReorder: false,
-			autoHeight: true
+			autoHeight: true,
+			editable: false
 		};
 
 		var dataView;
@@ -72,14 +79,34 @@
 			$("#dataGrid").on('click', 'a.del', function(e) {
 				deleteRow($(this).attr('itemId'));
 			})
+
+			$("#dataGrid").on('change', 'input.edit-quantity', function(e) {
+				
+
+				var itemId = $(this).attr('itemId');
+				var newVal = $(this).val() 
+
+				var item = dataView.getItemById(itemId);
+
+				if (item) {
+					item['quantity'] = newVal;
+					dataView.updateItem(item['id'], item)
+				}
+
+			})
+
 		}
 
 		function deleteLink(row, cell, value, columnDef, dataContext) {
-			return '<a class= "del" itemId=' + dataContext.id + ' href="javascript:void(0);"><img src="../icons/delete.png"></a>'
+			return '<a class="del" itemId=' + dataContext.id + ' href="javascript:void(0);"><img src="../icons/delete.png"></a>'
 		}
 
 		function statusIcon(row, cell, value, columnDef, dataContext) {
 			return '<img src="../icons/' + dataContext.status + '.png"/>'
+		}
+
+		function editQuantity(row, cell, value, columnDef, dataContext) {
+			return '<input class="edit-quantity" itemId=' + dataContext.id + ' type="text" value="' + dataContext.quantity + '">'
 		}
 
 		function addItems(newItems) {
@@ -120,10 +147,18 @@
 			}
 		}
 
+		function getItemQuantity(itemId) {
+			var item = dataView.getItemById(itemId);
+			if (item) {
+				return (item['quantity']);
+			}
+		}
+
 		$.extend(this, {
 			"addItems": addItems,
 			"getItems": getItems,
-			"updateItemStatus": updateItemStatus
+			"updateItemStatus": updateItemStatus,
+			"getItemQuantity": getItemQuantity
 		});
 
 		init();
